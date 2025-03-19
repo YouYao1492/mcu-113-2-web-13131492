8960da8be4f41334006952c4f73d0dc83990d5c0
陳宥耀 <13131492@me.mcu.edu.tw>
2025-03-19 17:49:11 +0800

    feat(ts): 利用物件記錄產品資料

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,17 @@ console.log("Create Date",createDate);
 
 let modifyDate :Date| undefined=undefined;
 console.log("Modify Date", modifyDate);
+
+
+const Product={
+    id:1,
+    name:"圖像 Angular 開發入門 第二版",
+    company:"博碩文化",
+    price:500,
+    isShow:true,
+    createDate:new Date(),
+    modifyDate:undefined,
+};
+
+console.log(Product);
+console.table(Product);
