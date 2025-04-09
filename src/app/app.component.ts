e274894af2091a823bc3b88661b3e920a2add2ab
陳宥耀 <13131492@me.mcu.edu.tw>
2025-04-09 18:00:43 +0800

    feat(product): 利用事件繫結設定產品顯示狀態

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { ProductCardComponent } from './product-card/product-card.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  productName = 'asdf';
-  author = 'asdf';
-  company = 'asdf';
+  productName = '書籍 A';
+  author = '作者甲、作者乙、作者丙';
+  company = '博碩文化';
   isShow = true;
-  photoUrl = 'asdf';
+  photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
 
   onSetDisplay(isShow: boolean): void {
     this.isShow = isShow;
