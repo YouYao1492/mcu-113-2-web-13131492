b3423c82b35978a91d220474e67807c76755c3d0
陳宥耀 <13131492@me.mcu.edu.tw>
2025-05-14 22:51:34 +0800

    feat(product): 實作產品名稱查詢作業

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,32 +1,45 @@
-import { Component, computed, inject, signal } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { Component, computed, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { rxResource, takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { Router } from '@angular/router';
 import { Product } from '../models/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { ProductService } from '../services/product.service';
 import { PaginationComponent } from './../pagination/pagination.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-product-page',
-  imports: [PaginationComponent, ProductCardListComponent],
+  imports: [ReactiveFormsModule, PaginationComponent, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit {
   private router = inject(Router);
 
   private productService = inject(ProductService);
+
+  private destroyRef = inject(DestroyRef);
+
+  readonly searchControl = new FormControl<string | undefined>(undefined, { nonNullable: true });
+
+  readonly productName = toSignal(
+    this.searchControl.valueChanges.pipe(debounceTime(500), distinctUntilChanged(), takeUntilDestroyed(this.destroyRef)),
+    {
+      initialValue: undefined,
+    }
+  );
 
   readonly pageIndex = signal(1);
 
   readonly pageSize = signal(5);
 
   private readonly data = rxResource({
-    request: () => ({ pageIndex: this.pageIndex(), pageSize: this.pageSize() }),
+    request: () => ({ name: this.productName(), pageIndex: this.pageIndex(), pageSize: this.pageSize() }),
     defaultValue: { data: [], count: 0 },
     loader: ({ request }) => {
-      const { pageIndex, pageSize } = request;
-      return this.productService.getList(undefined, pageIndex, pageSize);
+      const { name, pageIndex, pageSize } = request;
+      return this.productService.getList(name, pageIndex, pageSize);
     },
   });
 
@@ -39,6 +52,10 @@ export class ProductPageComponent {
     const { data } = this.data.value();
     return data;
   });
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges.subscribe((value) => console.log(value));
+  }
 
   onEdit(product: Product): void {
     this.router.navigate(['product', 'form', product.id]);
