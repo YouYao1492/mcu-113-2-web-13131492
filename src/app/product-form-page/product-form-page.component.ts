f60d3bff84d342f228b6145d6da1936f2f549fb2
陳宥耀 <13131492@me.mcu.edu.tw>
2025-05-21 18:49:37 +0800

    feat(product-page): 產品表單新增作業實作

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,6 +1,7 @@
+import { ProductService } from './../services/product.service';
 import { JsonPipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, Data } from '@angular/router';
+import { ActivatedRoute, Data, Router } from '@angular/router';
 import { map } from 'rxjs';
 import { Product } from '../models/product';
 import { Form, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -12,7 +13,9 @@ import { Form, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validator
   styleUrl: './product-form-page.component.scss',
 })
 export class ProductFormPageComponent implements OnInit {
+  private readonly router = inject(Router);
   private readonly route = inject(ActivatedRoute);
+  private readonly ProductService = inject(ProductService);
 
   form = new FormGroup({
     id: new FormControl<string | null>(null),
@@ -20,6 +23,7 @@ export class ProductFormPageComponent implements OnInit {
     authors: new FormArray<FormControl<string | null>>([]),
     company: new FormControl<string | null>(null, { validators: [Validators.required] }),
     price: new FormControl<number | null>(null, { validators: [Validators.required] }),
+    isShow: new FormControl<boolean>(true, { nonNullable: true }),
   });
 
   product!: Product;
@@ -36,6 +40,10 @@ export class ProductFormPageComponent implements OnInit {
     return this.form.get('company') as FormControl<string | null>;
   }
 
+  get isShow(): FormControl<boolean> {
+    return this.form.get('isShow') as FormControl<boolean>;
+  }
+
   get price(): FormControl<string | null> {
     return this.form.get('price') as FormControl<string | null>;
   }
@@ -47,5 +55,22 @@ export class ProductFormPageComponent implements OnInit {
   onAddAuthor(): void {
     const formControl = new FormControl<string | null>(null, { validators: [Validators.required] });
     this.authors.push(formControl);
+  }
+
+  onCancel(): void {
+    this.router.navigate(['products']);
+  }
+
+  onSave(): void {
+    const formData = new Product({
+      name: this.name.value!,
+      authors: this.authors.value.map((author) => author!),
+      company: this.company.value!,
+      isShow: this.isShow.value,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date(),
+      price: +(this.price.value || '0'),
+    });
+    this.ProductService.add(formData).subscribe(() => this.router.navigate(['products']));
   }
 }
